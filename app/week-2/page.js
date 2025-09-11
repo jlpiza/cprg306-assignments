39a407ead7df1a26810855fe34319c824614482a
jlpiza <126796387+jlpiza@users.noreply.github.com>
2025-09-11 10:44:08 -0600

    tested in class

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+    return (
+        <main>
+            <h1>Week 2 - Assignment </h1>
+            <StudentInfo />
+            </main>
+            );
+            }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+export default function Page() {
+
+  return (
+    <main>
+      <h1>Jessica Piza</h1>
+      <a
+        href="https://github.com/jlpiza/cprg306-assignments"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        My GitHub repository
+      </a>
+    </main>
+  );
+}
