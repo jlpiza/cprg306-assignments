f3cf6e0fd21eaaec3c7282002ebef4400686ce58
jlpiza <126796387+jlpiza@users.noreply.github.com>
2025-09-18 08:41:17 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Page() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
 
-      <Link href="/week-2">Week 2 Assignment</Link>
-      <Link href="/week-3">Week 2 Assignment</Link>
+      <Link href="/week-2">Week 2 Assignment</Link><br/>
+      <Link href="/week-3">Week 3 Assignment</Link>
 
 
     </main>
