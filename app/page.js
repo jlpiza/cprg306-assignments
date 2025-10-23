bfb3fb55c81bc82abb1cd09a269292e922256bf5
jlpiza <126796387+jlpiza@users.noreply.github.com>
2025-10-23 08:24:34 -0600

    add week 7 on main page

--- app/page.js
+++ app/page.js
@@ -13,6 +13,8 @@ export default function Page() {
       <Link href="/week-5">Week 5 Assignment</Link>
       <br />
       <Link href="/week-6">Week 6 Assignment</Link>
+            <br />
+      <Link href="/week-7">Week 7 Assignment</Link>
     </main>
   );
 }
